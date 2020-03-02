734de6caf4ec1622043f8a01c7c16d2b7fb12a8a
david_tojo <33786007+t19d@users.noreply.github.com>
2020-03-02 19:41:06 +0100

    Add files via upload

--- js/preguntas_frecuentes.js
+++ js/preguntas_frecuentes.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
     $(".pregunta").click(function () {
         let img = $(this).children("img");
-        if (img.attr("src") == "../assets/img/icons/arrow_down_black.png") {
+        if (img.attr("src") == "../assets/img/icons/arrow_down_white.png") {
             //Resetear el slide a todas las preguntas
             $(".respuesta").slideUp();
 
             $(".respuesta").prev().children("img").attr("src", 
-            $(".respuesta").prev().children("img").attr("src").replace("../assets/img/icons/arrow_up_black.png",
-                "../assets/img/icons/arrow_down_black.png"));
+            $(".respuesta").prev().children("img").attr("src").replace("../assets/img/icons/arrow_up_white.png",
+                "../assets/img/icons/arrow_down_white.png"));
 
             //Cambiar el icono
-            img.attr("src", img.attr("src").replace("../assets/img/icons/arrow_down_black.png",
-                "../assets/img/icons/arrow_up_black.png"));
+            img.attr("src", img.attr("src").replace("../assets/img/icons/arrow_down_white.png",
+                "../assets/img/icons/arrow_up_white.png"));
         } else {
             //Cambiar el icono
-            img.attr("src", img.attr("src").replace("../assets/img/icons/arrow_up_black.png",
-                "../assets/img/icons/arrow_down_black.png"));
+            img.attr("src", img.attr("src").replace("../assets/img/icons/arrow_up_white.png",
+                "../assets/img/icons/arrow_down_white.png"));
         }
         $(this).next().slideToggle();
     });
