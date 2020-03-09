c3611b74cdb5c2b5b8a77f5cfc08b4855719f065
andresfi33 <32295661+andresfi33@users.noreply.github.com>
2020-03-09 18:56:35 +0100

    Media Query Imagenes(Revisar)

--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,9 @@
+if (window.matchMedia("(min-width: 400px)").matches) {
+  /* La pantalla tiene al menos 400 píxeles de ancho */
+} else {
+  /* La pantalla tiene menos de 400 píxeles de ancho */
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -10,6 +16,7 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
+    if (window.matchMedia("(min-width: 767px)").matches) {
     var i;
     var slides = document.getElementsByClassName("imagenesSlideshow");
     var puntoPosicionSlideshows = document.getElementsByClassName("puntoPosicionSlideshow");
@@ -28,15 +35,16 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     puntoPosicionSlideshows[slideIndex - 1].className += " imagenActivaSlideshow";
 }
+}
 
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
-}
+}*/
 
 $(document).ready(function () {
     $(".izquierda").click(function () {
